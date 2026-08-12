0bab77c815c66c7e3739cc66800408e97990b5ba
Harsh Pandey <harshxvinod0331@gmail.com>
2026-08-12 10:46:20 +0530

    Method overloading

--- pointers.cpp
+++ pointers.cpp
@@ -8,9 +8,20 @@ void wish(const std::string& s = "default" , const int& age = 5){
     cout<<"happy birthday "<<s<<"for your "<<age<<"th birthday"<<'\n';
 }
 
+int add(int a , int b){
+    return a+b;
+}
+
+int add(int a , int c , int b){
+    return (a+b+c);
+}
+
 int main(){
     wish();
     wish();
+
+    cout<<"the addition of 2 and 3 is "<<add(2,3)<<'\n';
+    cout<<"the addition of 3,4,5 is "<<add(3,4,5)<<'\n';
     return 0;
 
 }
