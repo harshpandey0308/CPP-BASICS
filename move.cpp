5c18370ba2b0971099ca6924add2a6abb54d35bd
Harshpandey <208502446+harshpandey0308@users.noreply.github.com>
2026-09-23 05:09:06 +0000

    Move semantics

---
+++ move.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class EMPLOYEE{
+    private : 
+        string name;
+        string position;
+        double *salary;
+
+    public : 
+        EMPLOYEE(string name_ , string position_ , double salary_) : name(name_) , position(position_) , salary(new double(salary_)){}
+
+        EMPLOYEE(const EMPLOYEE& other) : name(other.name) , position(other.position){
+            marks(new double(other.salary));
+            *marks = *other.marks;
+            cout<<"copy constructor"<<'\n';
+        }
+
+        EMPLOYEE(EMPLOYEE&& other): name(other.name) , position(other.position) , salary(other.salary){
+                other.salary = nullptr;
+                cout<<"the pointer moved from other to new"<<'\n';
+        }
+
+        ~EMPLOYEE(){
+            delete salary;
+        }
+}
+
+int main(){
+    EMPLOYEE e1("harsh" , "Software engineer" , 50000.89);
+
+    EMPLOYEE e2 = e1;
+
+    EMPLOYEE e3 = e1;
+}
+
